a8ee28d80d50b49b95049442e814c2529ba31ac1
Totto16 <tobiausgais@gmail.com>
2025-12-08 00:38:10 +0100

    fix: update the state with the indeterminate state a few times, and the indeterminate bar moves

--- languages/c/src/example.c
+++ languages/c/src/example.c
@@ -9,12 +9,19 @@ static __useconds_t seconds_to_us(double seconds) {
 }
 
 int main(void) {
+  const size_t percentage = 100;
+
+  send_progress((ProgressReport){.type = ProgressStateRemove});
 
   {
+    const size_t steps = 50;
     fprintf(stderr, "Indeterminate progress bar for 2 seconds\n");
-    send_progress((ProgressReport){.type = ProgressStateIndeterminate});
 
-    usleep(seconds_to_us(2.0));
+    for (size_t i = 0; i <= steps; ++i) {
+      send_progress((ProgressReport){.type = ProgressStateIndeterminate});
+
+      usleep(seconds_to_us(2.0 / ((double)steps)));
+    }
   }
 
   send_progress((ProgressReport){.type = ProgressStateRemove});
@@ -22,22 +29,26 @@ int main(void) {
   {
     fprintf(stderr, "Progress bar from 0%% to 100%% in 5 seconds\n");
 
-    for (size_t i = 0; i <= 100; ++i) {
+    for (size_t i = 0; i <= percentage; ++i) {
       send_progress(
           (ProgressReport){.type = ProgressStateSet, .data = {.set = i}});
 
-      usleep(seconds_to_us(5.0 / 100.0));
+      usleep(seconds_to_us(5.0 / ((double)percentage)));
     }
   }
 
   send_progress((ProgressReport){.type = ProgressStateRemove});
 
   {
+    const size_t steps = 50;
     fprintf(stderr, "Error progress bar for 2 seconds\n");
-    send_progress((ProgressReport){.type = ProgressStateError,
-                                   .data = {.error = {.has_value = false}}});
 
-    usleep(seconds_to_us(2.0));
+    for (size_t i = 0; i <= steps; ++i) {
+      send_progress((ProgressReport){.type = ProgressStateError,
+                                     .data = {.error = {.has_value = false}}});
+
+      usleep(seconds_to_us(2.0 / ((double)steps)));
+    }
   }
 
   send_progress((ProgressReport){.type = ProgressStateRemove});
@@ -45,23 +56,27 @@ int main(void) {
   {
     fprintf(stderr, "Progress bar error from 0%% to 100%% in 5 seconds\n");
 
-    for (size_t i = 0; i <= 100; ++i) {
+    for (size_t i = 0; i <= percentage; ++i) {
       send_progress(
           (ProgressReport){.type = ProgressStateError,
                            .data = {.error = {.has_value = true, .value = i}}});
 
-      usleep(seconds_to_us(5.0 / 100.0));
+      usleep(seconds_to_us(5.0 / ((double)percentage)));
     }
   }
 
   send_progress((ProgressReport){.type = ProgressStateRemove});
 
   {
+    const size_t steps = 50;
     fprintf(stderr, "Paused progress bar for 2 seconds\n");
-    send_progress((ProgressReport){.type = ProgressStatePaused,
-                                   .data = {.paused = {.has_value = false}}});
 
-    usleep(seconds_to_us(2.0));
+    for (size_t i = 0; i <= steps; ++i) {
+      send_progress((ProgressReport){.type = ProgressStatePaused,
+                                     .data = {.paused = {.has_value = false}}});
+
+      usleep(seconds_to_us(2.0 / ((double)steps)));
+    }
   }
 
   send_progress((ProgressReport){.type = ProgressStateRemove});
@@ -69,12 +84,12 @@ int main(void) {
   {
     fprintf(stderr, "Progress bar paused from 0%% to 100%% in 5 seconds\n");
 
-    for (size_t i = 0; i <= 100; ++i) {
+    for (size_t i = 0; i <= percentage; ++i) {
       send_progress((ProgressReport){
           .type = ProgressStatePaused,
           .data = {.paused = {.has_value = true, .value = i}}});
 
-      usleep(seconds_to_us(5.0 / 100.0));
+      usleep(seconds_to_us(5.0 / ((double)percentage)));
     }
   }
 
--- languages/zig/src/example.zig
+++ languages/zig/src/example.zig
@@ -6,15 +6,22 @@ fn seconds_to_us(seconds: f64) u64 {
 }
 
 pub fn main() !void {
-    var buffer: [terminal_progress.buffer_length]u8 = undefined;
+    const percentage = 100;
 
+    var buffer: [terminal_progress.buffer_length]u8 = undefined;
     var writer = terminal_progress.ProgressWriter.create(&buffer);
 
-    {
-        std.debug.print("Indeterminate progress bar for 2 seconds\n", .{});
-        try writer.setProgress(.indeterminate);
+    try writer.setProgress(.remove);
 
-        std.Thread.sleep(seconds_to_us(2.0));
+    {
+        const steps = 50;
+        std.debug.print("Indeterminate progress bar for 2 seconds\n", .{});
+
+        for (0..steps + 1) |_| {
+            try writer.setProgress(.indeterminate);
+
+            std.Thread.sleep(seconds_to_us(2.0 / @as(f64, @floatFromInt(steps))));
+        }
     }
 
     try writer.setProgress(.remove);
@@ -22,19 +29,23 @@ pub fn main() !void {
     {
         std.debug.print("Progress bar from 0% to 100% in 5 seconds\n", .{});
 
-        for (0..101) |i| {
+        for (0..percentage + 1) |i| {
             try writer.setProgress(.{ .set = @intCast(i) });
-            std.Thread.sleep(seconds_to_us(5.0 / 100.0));
+            std.Thread.sleep(seconds_to_us(5.0 / @as(f64, @floatFromInt(percentage))));
         }
     }
 
     try writer.setProgress(.remove);
 
     {
+        const steps = 50;
         std.debug.print("Error progress bar for 2 seconds\n", .{});
-        try writer.setProgress(.{ .@"error" = null });
 
-        std.Thread.sleep(seconds_to_us(2.0));
+        for (0..steps + 1) |_| {
+            try writer.setProgress(.{ .@"error" = null });
+
+            std.Thread.sleep(seconds_to_us(2.0 / @as(f64, @floatFromInt(steps))));
+        }
     }
 
     try writer.setProgress(.remove);
@@ -42,19 +53,23 @@ pub fn main() !void {
     {
         std.debug.print("Progress bar error from 0% to 100% in 5 seconds\n", .{});
 
-        for (0..101) |i| {
+        for (0..percentage + 1) |i| {
             try writer.setProgress(.{ .@"error" = @intCast(i) });
-            std.Thread.sleep(seconds_to_us(5.0 / 100.0));
+            std.Thread.sleep(seconds_to_us(5.0 / @as(f64, @floatFromInt(percentage))));
         }
     }
 
     try writer.setProgress(.remove);
 
     {
+        const steps = 50;
         std.debug.print("Paused progress bar for 2 seconds\n", .{});
-        try writer.setProgress(.{ .paused = null });
 
-        std.Thread.sleep(seconds_to_us(2.0));
+        for (0..steps + 1) |_| {
+            try writer.setProgress(.{ .paused = null });
+
+            std.Thread.sleep(seconds_to_us(2.0 / @as(f64, @floatFromInt(steps))));
+        }
     }
 
     try writer.setProgress(.remove);
@@ -62,9 +77,9 @@ pub fn main() !void {
     {
         std.debug.print("Progress bar paused from 0% to 100% in 5 seconds\n", .{});
 
-        for (0..101) |i| {
+        for (0..percentage + 1) |i| {
             try writer.setProgress(.{ .paused = @intCast(i) });
-            std.Thread.sleep(seconds_to_us(5.0 / 100.0));
+            std.Thread.sleep(seconds_to_us(5.0 / @as(f64, @floatFromInt(percentage))));
         }
     }
 
